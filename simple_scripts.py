23e294220772fa1d7476ada8738733a14d140602
Md. Jabed Ahmed <jabedahmedrifat1@gmail.com>
2023-09-19 15:59:47 +0600

    fixed

--- simple_scripts.py
+++ simple_scripts.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 api_key = "b1c6b1d4eee69018389a08305b8e88cd"
 
 
-base_url = "https://api.themoviedb.org/3/discover/movie?language=en-US&region=IN&with_original_language=hi"
+base_url = "https://api.themoviedb.org/3/movie/top_rated?language=en-US"
 
 
 end_date = datetime.now().date()
