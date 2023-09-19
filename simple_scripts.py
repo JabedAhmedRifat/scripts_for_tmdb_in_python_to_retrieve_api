ebbec70467010ef961b6c572194aef2ed6f35d6d
Md. Jabed Ahmed <jabedahmedrifat1@gmail.com>
2023-09-19 15:47:11 +0600

    simple scripts

--- simple_scripts.py
+++ simple_scripts.py
@@ -4,30 +4,27 @@ from datetime import datetime, timedelta
 
 api_key = "b1c6b1d4eee69018389a08305b8e88cd"
 
-# Construct the URL using the TMDb ID
-base_url = "https://api.themoviedb.org/3/trending/movie/week"
+
+base_url = "https://api.themoviedb.org/3/discover/movie?language=en-US&region=IN&with_original_language=hi"
 
 
 end_date = datetime.now().date()
 
 start_date = end_date - timedelta(days=7) 
 
-# Define your API parameters, if needed
+
 params = {
     "api_key": api_key,
     "primary_release_date.gte": None,  
     "primary_release_date.lte": start_date,
     
     }
-    # Add any other parameters you need here
+    
 
 response = requests.get(base_url, params=params)
 
 if response.status_code == 200:
     movie_data = response.json()
-    # print("Movie Details:")
-    # print("Title:", movie_data.get("title"))
-    # print("Release Date:", movie_data.get("release_date"))
     print(movie_data)
     
 else:
